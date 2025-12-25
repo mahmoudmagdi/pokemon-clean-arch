58fc81ae4406a7da814d19215390e7b282ed3926
Mahmoud ElKhlafawi <mahmoudmagdi@ymail.com>
2025-12-25 03:36:07 +0200

    Add HttpError class and httpJson utility function for API requests

---
+++ libs/presentation/utils/http.ts
@@ -0,0 +1,34 @@
+export class HttpError extends Error {
+    constructor(
+        message: string,
+        public status: number,
+        public body?: unknown
+    ) {
+        super(message);
+        this.name = "HttpError";
+    }
+}
+
+export async function httpJson<T>(input: RequestInfo | URL, init?: RequestInit): Promise<T> {
+    const res = await fetch(input, {
+        ...init,
+        headers: {
+            "Accept": "application/json",
+            ...(init?.headers ?? {})
+        }
+    });
+
+    let body: unknown = undefined;
+    const contentType = res.headers.get("content-type") ?? "";
+    if (contentType.includes("application/json")) {
+        body = await res.json();
+    } else {
+        body = await res.text();
+    }
+
+    if (!res.ok) {
+        throw new HttpError(`HTTP ${res.status}`, res.status, body);
+    }
+
+    return body as T;
+}
